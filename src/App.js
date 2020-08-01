b1a3aae2163ae48c4ced826e75de4546b6130514
unknown <keneson@bol.com.br>
2020-07-31 23:30:18 -0300

    setup inicial

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carrossel from './components/Carousel';
+import Rodapé from './components/Footer';
+
+function App() {
+  return (
+    <div style ={{ background :"#141414"}}>
+
+      <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área de termos HTML, CSS e JavaScript fazem parte da rotina dos desenvolvedores e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+      />
+
+< Carrossel
+        ignoreFirstVideo
+        category = { dadosIniciais . categorias [0] }
+      />
+
+      < Carrossel
+        category = { dadosIniciais . categorias [1] }
+      />
+
+      < Carrossel
+        category = { dadosIniciais . categorias [2] }
+      />      
+
+      < Carrossel
+        category = { dadosIniciais . categorias [3] }
+      />      
+
+      < Carrossel
+        category = { dadosIniciais . categorias [4] }
+      />      
+
+      < Carrossel
+        category = { dadosIniciais . categorias [5] }
+      />      
+
+      < Rodapé />
+
+    
+
+
+    </div>
+  );
+}
+
+export default App;
