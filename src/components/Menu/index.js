6043bbe47cf01cc34b301fcd98a2aebdbb8da84c
keneson2309 <keneson@bol.com.br>
2020-10-07 10:43:54 -0300

    trocando o nome

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/Logo.png';
+import Logo from '../../assets/img/kenesonflix.png';
 import './Menu.css';
-//import ButtonLink from './components/ButtonLink';
-import Button from '../Button'
-function Menu(){
-    return(
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="KenesonFlix logo" />
-            </Link>
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Video
-            </Button>
-        </nav>
-    )
-}
+// import ButtonLink from './components/ButtonLink'
+import Button from '../Button';
 
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="KenesonFlix logo" />
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Video
+      </Button>
+    </nav>
+  );
+}
 export default Menu;
